e5d784171c2bceae92148c2be1e63b8ad94fd833
Autumn <autumncbullard@gmail.com>
2018-10-20 10:23:20 -0400

    Make a POST request that creates a shortened URL and saves it to the database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const validURL = require('valid-url');
+const randomstring = require('randomstring');
 const ShortURL = require('./models/shorturl.js');
 
 const app = express();
@@ -15,6 +17,35 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static(`${__dirname}/public`));
+
+app.post('/api/shorturl/new', (req, res) => {
+  let alias,
+  shortURL;
+
+  if (validURL.isUri(req.body.url)) {
+    alias = randomstring.generate({
+      length: 8,
+      charset: 'alphanumeric'
+    });
+    shortURL = new ShortURL({
+      url: req.body.url,
+      alias
+    });
+    shortURL.save().then(url => {
+      res.json({
+        original_url: req.body.url,
+        short_url: `https://autumnchris-url-shortener.herokuapp.com/${alias}`
+      });
+    }).catch(error => {
+      res.json({ error });
+    });
+  }
+  else {
+    res.json({
+      error: 'Invalid URL'
+    });
+  }
+});
 
 app.use((req, res) => {
   res.sendFile(`${__dirname}/views/404.html`, 404);
