7297ded6df72a56f0392af4e3055049006cf76a3
Autumn <autumncbullard@gmail.com>
2018-10-20 00:29:59 -0400

    Connect to the API's MongoDB database using Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
