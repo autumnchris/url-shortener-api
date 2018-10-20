7ca9709b1b1f97f1a0b9ac0aa261e572aa50244d
Autumn <autumncbullard@gmail.com>
2018-10-20 00:24:13 -0400

    Require body-parser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
