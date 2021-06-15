e03c8bac673b2201a51d229f6a192fed638c59da
autumnchris <autumncbullard@gmail.com>
2021-06-15 17:13:31 -0400

    Fix Mongoose deprecation warnings

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 app.use(express.static(`${__dirname}/public`));
 
